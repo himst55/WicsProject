3fc8eeaaf57802a5726c75ae48b90d1bd81a6776
Tejaswi Thapa <tejaswi.thapa17@gmail.com>
2024-02-03 14:17:57 -0600

    Updated HomePage.js

--- wics_app/src/App.js
+++ wics_app/src/App.js
@@ -2,17 +2,23 @@ import Navbar from "./Components/Navbar"
 import { Routes, Route } from 'react-router-dom'
 import FindSW from './pages/FindSW'
 import GetNumber from './pages/GetNumber'
+import HomePage from './pages/HomePage'
 
 function App() {
-  return (
-    <div>
-      <Routes>
-        <Route path="/findsw" component={FindSW} />
-        <Route path="/getnum" component={GetNumber} />
-      </Routes>
-      <Navbar></Navbar>
-    </div>
-  );
+   return (
+      <div>
+         <Routes>
+            <Route path="/findsw" component={FindSW} />
+            <Route path="/getnum" component={GetNumber} />
+         </Routes>
+         <Navbar></Navbar>
+         <div className="Home">
+            <header>
+               <HomePage />
+            </header>
+         </div>
+      </div>
+   );
 }
 
 export default App;
--- wics_app/src/pages/HomePage.js
+++ wics_app/src/pages/HomePage.js
@@ -1,17 +1,29 @@
 import React from 'react';
-import background from "./Background.png";
+import background from "../Background.png";
 
 const HomePage = () => {
-   const myStyle = {
+   const imageStyle = {
       backgroundImage: `url(${background})`,
       height: "100vh",
-      marginTop: "-70px",
-      fontSize: "50px",
+      marginTop: "-80px",
       backgroundSize: "cover",
-      backgroundRepeat: "no-repeat",   };
+      backgroundRepeat: "no-repeat",
+   };
+   const headerStyle = {
+      fontSize: "60px",
+      padding: "30px 0px 0px 45px",
+      color: "#00608a",
+   };
+   const textStyle = {
+      fontSize: "20px",
+      padding: "0px 0px 0px 45px",
+      align: "top",
+      color: "#00608a",
+   };
    return (
-      <div style={myStyle}>
-         <h4>Where is my Sure Walk?</h4>
+      <div style={imageStyle}>
+         <h4 style={headerStyle}>Where's my Sure Walk?</h4>
+         <p style={textStyle}>Use your Sure Walk number to track how far away your Sure Walk is!</p>
       </div>
    );
 }
